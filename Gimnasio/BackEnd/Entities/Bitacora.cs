d6a83dd20aa6ac70c26e8f0634f293cd72a17fd6
W16Il <58409291+W16Il@users.noreply.github.com>
2021-04-11 16:31:59 -0600

    Agregando las plantilla

--- Gimnasio/BackEnd/Entities/Bitacora.cs
+++ Gimnasio/BackEnd/Entities/Bitacora.cs
@@ -8,10 +8,10 @@ namespace BackEnd.Entities
     public partial class Bitacora
     {
         public int IdBitacora { get; set; }
+        public int? IdUsuario { get; set; }
         public string Descripcion { get; set; }
         public string Modulo { get; set; }
         public DateTime? Fecha { get; set; }
-        public int? IdUsuario { get; set; }
 
         public virtual Usuario IdUsuarioNavigation { get; set; }
     }
---
+++ Gimnasio/BackEndAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,116 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndAPI.Controllers
+{
+    [Route("api/Usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetUsers()
+        {
+            try
+            {
+                IEnumerable<Usuario> usuarios;
+                using (var context = new UnidadDeTrabajo<Usuario>(new GimnasioContext()))
+                {
+                    usuarios = context.genericDAL.GetAll();
+                }
+                return new JsonResult(usuarios);
+            }
+            catch (Exception ex)
+            {
+                var s = ex.Message;
+                return new JsonResult(null);
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public JsonResult GetUser(int id)
+        {
+            try
+            {
+                Usuario usuario;
+                using (var context = new UnidadDeTrabajo<Usuario>(new GimnasioContext()))
+                {
+                    usuario = context.genericDAL.Get(id);
+                }
+                return new JsonResult(usuario);
+            }
+            catch (Exception ex)
+            {
+
+                var s = ex.Message;
+                return new JsonResult(null);
+            }
+
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateUser(Usuario usuario)
+        {
+            try
+            {
+                using var context = new UnidadDeTrabajo<Usuario>(new GimnasioContext());
+                context.genericDAL.Add(usuario);
+                return (context.Complete()) ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                var s = ex.Message;
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+
+        }
+
+
+        [HttpPatch]
+        public IActionResult UpdateUser(Usuario usuario)
+        {
+            try
+            {
+                using var context = new UnidadDeTrabajo<Usuario>(new GimnasioContext());
+                context.genericDAL.Update(usuario);
+                return (context.Complete()) ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                var s = ex.Message;
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+
+        }
+
+        /*
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+                using var context = new UnidadDeTrabajo<Usuario>(new GimnasioContext());
+                Usuario usuario = context.genericDAL.Get(id);
+                context.genericDAL.Remove(usuario);
+                return (context.Complete()) ? Ok() : StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                var s = ex.Message;
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+        }
+        */
+
+    }
+}
